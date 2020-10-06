2f38c184d1dadde8de74d3030d1f5a6b664061b0
tadasgrinevic <41401466+tadasgrinevic@users.noreply.github.com>
2020-10-06 13:16:53 +0100

    Update index.php
    In this version we are using XPath to read data from XML file.

--- index.php
+++ index.php
@@ -10,21 +10,19 @@ if(isset($argv[1]) && isset($argv[2]))
     $input = "example.xml";
     $output = "data/persons.csv";
 }
+  
 $xml = simplexml_load_file($input); // Loading XML file
 $f = fopen($output, 'w'); // CSV file to be saved
 
-function createCsv($xml,$f)
+function createCsv($xml, $f)
 {
     $row = array("generated id", "first name", "surname");
     fputcsv($f, $row , ";");
     $i=0;
-    foreach($xml->children() as $child) {
+    foreach($xml->xpath('/personnel/person') as $person) {
         $i++;
-        $key = $child->children()->getName();
-        if($key == "firstName") {
-            $child->firstName = ucfirst(strtolower(strrev($child->firstName))); // Reversing name with first letter capitalized
-        }
-        $row = array($i, $child->firstName, $child->surname);
+        $person->firstName = ucfirst(strtolower(strrev($person->firstName))); // Reversing name with first letter capitalized
+        $row = array($i, $person->firstName, $person->surname);
         fputcsv($f, $row , ";");
     }
 }
