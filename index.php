68c53de81069b3f95c289e994f62e035eb7e2d07
tadasgrinevic <41401466+tadasgrinevic@users.noreply.github.com>
2020-10-06 14:27:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,30 +3,60 @@
 // Checking if arguments were passed via bash script
 if(isset($argv[1]) && isset($argv[2]))
 {
-    var_dump($argv);
     $input = $argv[1].".xml";
     $output = "data/".$argv[2].".csv";
 } else {
     $input = "example.xml";
     $output = "data/persons.csv";
 }
-  
-$xml = simplexml_load_file($input); // Loading XML file
-$f = fopen($output, 'w'); // CSV file to be saved
 
-function createCsv($xml, $f)
+
+libxml_use_internal_errors(TRUE);
+$xml = simplexml_load_file($input); // Loading XML file
+
+if(!($xml instanceof SimpleXMLElement))
 {
-    $row = array("generated id", "first name", "surname");
-    fputcsv($f, $row , ";");
-    $i=0;
-    foreach($xml->xpath('/personnel/person') as $person) {
-        $i++;
-        $person->firstName = ucfirst(strtolower(strrev($person->firstName))); // Reversing name with first letter capitalized
-        $row = array($i, $person->firstName, $person->surname);
-        fputcsv($f, $row , ";");
+    $errors = libxml_get_errors();
+
+    $errorStr = "Invalid XML " . (!empty($errors)) ? "(" . count($errors) .
+                " errors returned):\n":'. Unknown error.' . "\n";
+
+    if(!empty($errors)){
+        foreach($errors as $key => $error)
+            $errorStr .= ($key + 1) . ")  " . $error->message . "\n";
     }
+
+    echo 'Error ' . $errorStr;
+    exit;
 }
 
-createCsv($xml, $f);
-fclose($f);
+
+function createCsv($xml, $output)
+{
+    $f = fopen($output, 'w'); // CSV file to be saved
+    $row = array("generated id", "first name", "surname");
+    fputcsv($f, $row , ";");
+
+    $i=0;
+
+    foreach($xml->xpath('/personnel/person') as $person) {
+        if(!empty($person->firstName) && !empty($person->surname)) // Check if First name and Surname are not empty
+        {
+            $i++;
+            $person->firstName = ucfirst(strtolower(strrev($person->firstName))); // Reversing name with first letter capitalized
+            $row = array($i, $person->firstName, $person->surname);
+            fputcsv($f, $row , ";");
+        }
+    }
+    if($i>0)
+    {
+        echo basename($output)." file saved with ".$i." rows!";
+    }
+    else {
+        echo "There is no data found to be saved to the ".basename($output)." file!";
+    }
+    fclose($f);
+}
+
+createCsv($xml, $output);
 ?>
